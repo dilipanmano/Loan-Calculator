55dc9e34abc9086b3d6b89bb09d7b9dfe408ab2a
Dilipan M <dilipan12m@gmail.com>
2022-08-27 01:26:39 +0530

    loan calculations

---
+++ app.js
@@ -0,0 +1,30 @@
+document.getElementById('loan-form').addEventListener('submit',calculate);
+
+function calculate(e){
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calcInterest = parseFloat(interest.value) / 100 / 12;
+    const calcYears = parseFloat(years.value) * 12;
+
+    const x = Math.pow(1+calcInterest,calcYears);
+    const monthly = (principal*x*calcInterest)/(x-1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calcYears).toFixed(2);
+        totalInterest.value = ((monthly * calcYears) - principal).toFixed(2);
+    }else{
+        console.log('Incorrect numbers');
+        callError('Incorrect Numbers');
+    }
+    e.preventDefault();
+}
+
+
+
